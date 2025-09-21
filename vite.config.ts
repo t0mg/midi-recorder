063fef12014dcd46d99d7bc17949169d8104fdeb
t0mg <t0mg@users.noreply.github.com>
2025-09-21 10:47:53 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,8 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/web-midi-recorder/',
+      base: '/midi-recorder/',
       plugins: [],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
